c7ae139094e3ad04786f2544351f27464e9b855f
Fairoz <64626697+fairoz97@users.noreply.github.com>
2026-02-08 20:58:31 +0800

    Add files via upload

---
+++ finance tracker/sw.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title></title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="2685.3">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica; -webkit-text-stroke: #000000}
+    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica; -webkit-text-stroke: #000000; min-height: 14.0px}
+    span.s1 {font-kerning: none}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="s1">const CACHE_NAME = "finance-tracker-v1";</span></p>
+<p class="p1"><span class="s1">const ASSETS = [</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>"./",</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>"./finance-tracker.html",</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>"./manifest.webmanifest",</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>"./icons/icon-192.png",</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>"./icons/icon-512.png"</span></p>
+<p class="p1"><span class="s1">];</span></p>
+<p class="p2"><span class="s1"></span><br></p>
+<p class="p1"><span class="s1">self.addEventListener("install", (event) =&gt; {</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>event.waitUntil(caches.open(CACHE_NAME).then((cache) =&gt; cache.addAll(ASSETS)));</span></p>
+<p class="p1"><span class="s1">});</span></p>
+<p class="p2"><span class="s1"></span><br></p>
+<p class="p1"><span class="s1">self.addEventListener("fetch", (event) =&gt; {</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>event.respondWith(</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>caches.match(event.request).then((cached) =&gt; cached || fetch(event.request))</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>);</span></p>
+<p class="p1"><span class="s1">});</span></p>
+</body>
+</html>
